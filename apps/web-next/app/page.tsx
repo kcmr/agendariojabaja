2976228a63c253be321e69753c4f1af2e244cc29
Kus Cámara <kcmr@users.noreply.github.com>
2026-05-03 20:11:07 +0200

    rename web to web-next

--- apps/web-next/app/page.tsx
+++ apps/web-next/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
+            Get started by editing <code>apps/web-next/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
           <li>5 + 4 is {add(5, 4)}</li>
@@ -65,7 +65,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
+        <Button appName="web-next" className={styles.secondary}>
           Open alert
         </Button>
       </main>
